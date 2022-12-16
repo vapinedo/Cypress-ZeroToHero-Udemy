83d764b036a9ac72c4ab1a112f83dc85b93925f5
Victor Pinedo <victor.pinedo@sailpoint.com>
2022-12-16 08:15:23 -0500

    12. Finding Web Elements

--- cypress/e2e/firstTest.spec.ts
+++ cypress/e2e/firstTest.spec.ts
@@ -37,4 +37,24 @@ describe('Our first suite', () => {
         // 10. The most recommended way by Cypress
         cy.get('[data-cy="imputEmail1"]');
     });
+
+    it.only('second test', () => {
+        cy.visit('/');
+        cy.contains('Forms').click();
+        cy.contains('Form Layouts').click();
+
+        cy.get('[data-cy="signInButton"]');
+        cy.contains('Sign in');
+        cy.contains('[status="warning"]', 'Sign in');
+
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click();
+
+        cy.contains('nb-card', 'Horizontal form').find('[type="email"]');
+    });
 });
