68640692cc431e8c0f0eb26e04918d449f7b2ef2
Victor Pinedo <victor.pinedo@sailpoint.com>
2022-12-19 20:41:55 -0500

    14. Invoke Command

--- cypress/e2e/firstTest.spec.ts
+++ cypress/e2e/firstTest.spec.ts
@@ -58,7 +58,7 @@ describe('Our first suite', () => {
         cy.contains('nb-card', 'Horizontal form').find('[type="email"]');
     });
 
-    it.only('then and wrap', () => {
+    it('then and wrap', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
@@ -92,5 +92,49 @@ describe('Our first suite', () => {
                 cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
             })
         })
+    });
+
+    it('invoke command', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // ways to find text on DOM
+        // # 1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        // # 2
+        cy.get('[for="exampleInputEmail1"]').then(label => {
+            expect(label.text()).to.equal('Email address')
+        })
+
+        // # 3
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+            expect(text).to.equal('Email address')
+        })
+
+        cy.contains('nb-card', 'Basic form')
+            .find('nb-checkbox')
+            .click()
+            .find('.custom-checkbox')
+            .invoke('attr', 'class')
+            // .should('contain', 'checked')
+            .then(classValue => {
+                expect(classValue).to.contain('checked')
+            })
+    });
+
+    it.only('assert property', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker')
+            .find('input')
+            .then(input => {
+                cy.wrap(input).click()
+                cy.get('nb-calendar-day-picker').contains('17').click() // 17 since we'll select 17 as day date
+                cy.wrap(input).invoke('prop', 'value').should('contain', 'Dec 17, 2022')
+            })
     });
 });
